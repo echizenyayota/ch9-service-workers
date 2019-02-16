b5855d33aabcb2bfbf2ae73ab37b42a58194be52
echizenyayota <virtualmisso30@gmail.com>
2019-02-16 19:09:27 +0900

    リスト9.2 サービスワーカーのinstallイベントでアセットをキャッシュする

--- htdocs/sw.js
+++ htdocs/sw.js
@@ -1,1 +1,25 @@
 // Service worker code goes in here!
+var cacheVersion = "v1",
+    cachedAssets = [
+        "/css/global.css",
+        "/js/debounce.js",
+        "/js/nav.js",
+        "/js/attach-nav.js",
+        "/img/global/jeremy.svg",
+        "/img/global/icon-github.svg",
+        "/img/global/icon-email.svg",
+        "/img/global/icon-twitter.svg",
+        "/img/global/icon-linked-in.svg",
+    ];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(chashes.open(cacheVersion).then(function(cache) {
+        return chache.addAll(cachedAssets);
+    }).then(function() {
+        return self.skipWainting();
+    }));
+});
+
+self.addEventListener("activate", function(){
+    return self.clients.claim();
+});
